fd030c2a3bc855fb1065a8cf29c3c1a049b1e60e
dbujold <david.bujold@mail.mcgill.ca>
2016-02-18 13:02:02 -0500

    Added script to call all wrappers in one operation, with assembly and taxon_id provided.

---
+++ IHEC_json_converter/fetch_all_exp_jsons.py
@@ -0,0 +1,72 @@
+import sys
+import getopt
+import json
+from datetime import datetime
+import rnaseq, bisulfite, chipseq
+
+def main(argv):
+    opts, args = getopt.getopt(argv, "", ["assembly=", "taxon-id="])
+
+    assembly = ''
+    taxon_id = ''
+    for opt, arg in opts:
+        if opt == '--assembly':
+            assembly = arg
+        if opt == '--taxon-id':
+            taxon_id = arg
+
+    if assembly == '':
+        raise Exception("Missing assembly parameter.")
+    if taxon_id == '':
+        raise Exception("Missing taxon id.")
+
+    date_str = datetime.now().date()
+
+    #Todo: Merge experiments as a single JSON
+
+    #Whole-Genome Bisulfite Sequencing experiments
+    print("Processing WGB-Seq...")
+    try:
+        data = bisulfite.bisulfite_wrapper(assembly='hg19', taxon_id=9606)
+        filename = 'WGB-Seq_%s_%s_%s.json' % (taxon_id, assembly, date_str)
+        output_file(data, filename)
+        print("Done.")
+    except Exception as e:
+        print('An error occured while fetching WGB-Seq experiments: ' + e.message)
+    print
+
+    #RNA-Sequencing experiments
+    print("Processing RNA-Seq...")
+    try:
+        data = rnaseq.rna_seq_wrapper(assembly=assembly, taxon_id=taxon_id)
+        filename = 'RNA-Seq_%s_%s_%s.json' % (taxon_id, assembly, date_str)
+        output_file(data, filename)
+        print("Done.")
+    except Exception as e:
+        print('An error occured while fetching RNA-Seq experiments: ' + e.message)
+    print
+
+    #ChIP-Seq experiments
+    targets = ['H3K27ac', 'H3K27me3', 'H3K36me3', 'H3K4me1', 'H3K4me3', 'H3K9me3']
+    for t in targets:
+        print("Processing ChIP-Seq %s..." % t)
+        try:
+            data = chipseq.chip_seq_wrapper(assembly='hg19', taxon_id=9606, target=t)
+            filename = 'ChIP-Seq_%s_%s_%s_%s.json' % (taxon_id, assembly, t, date_str)
+            output_file(data, filename)
+            print("Done.")
+        except Exception as e:
+            print('An error occured while fetching ChIP-Seq %s experiments: ' % t + e.message)
+        print
+    print("Operation completed.")
+
+
+
+def output_file(data, filename):
+    with open('../output/%s' % (filename), 'w+') as outfile:
+        json.dump(data, outfile, indent=4)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
